31a00455180fda71e48f318024ae54e029df10c1
nikhilcusc <42551852+nikhilcusc@users.noreply.github.com>
2019-03-01 17:26:05 -0800

    MultiChild Tree with BFS

---
+++ TreeBasics/NormalTree.py
@@ -0,0 +1,37 @@
+class TreeNode:
+    def __init__(self,value):
+        self.val = value
+        self.Children = []
+        self.ChildrenCount = 0
+
+
+class Tree:
+    def __init__(self, rootval):
+        self.val = rootval
+        self.Children = []
+        self.ChildrenCount = 0
+
+
+    def insertchild(self, value):
+        Child = Tree(value)
+
+        self.Children.append(Child)
+        self.ChildrenCount += 1
+        return Child
+
+
+def BFS(node):
+    BFSlist = []
+    queue1 = [node]
+    print("At node",node.val, "with children ",node.Children)
+    while queue1:
+        print("in while with queue", queue1)
+        curNode = queue1[0]
+        BFSlist.append(curNode.val)
+        queue1=queue1[1:]
+        for child in curNode.Children:
+            queue1.append(child)
+    return BFSlist
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from TreeBasics import BST
-from TreeBasics import node
+#from TreeBasics import node
+from TreeBasics import NormalTree
 
 
-
-T1 = BST.BST()
+T1 = NormalTree.Tree(74)
 lsit = [56,76,3,22,96,324,5,32,94]
-for l in lsit:
-    T1.insert(l)
-T1.printTree()
+r1 = T1.insertchild(56)
+l1 = T1.insertchild(76)
+
+r11 = r1.insertchild(3)
+r12 = r1.insertchild(22)
+r13 = r1.insertchild(96)
+
+l11 = l1.insertchild(324)
+l12 = l1.insertchild(5)
+l13 = l1.insertchild(32)
+
+#T1.printTree()
+print("Children of 56",r1.ChildrenCount)
+for i in r1.Children:
+    print(i.val)
+
+print("BFS list is : ",NormalTree.BFS(T1))
+
+
+#print("DFS list is : ",NormalTree.DFS(T1))
